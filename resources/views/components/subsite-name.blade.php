4b0086773eb28dd6e9b4ca7f4185e392a8f2d09e
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-13 08:43:08 -0500

    roadmap

--- resources/views/components/subsite-name.blade.php
+++ resources/views/components/subsite-name.blade.php
@@ -1,3 +1,7 @@
+{{--Roadmap:--}}
+{{--Have some context here. Basically, if teams are our only tenent, then our team is our subsite.  --}}
+{{--But, if we have tenancy, like a school or club name, then we'll want some different logic--}}
+{{--We'll add the extra subsite logic (somewhere) when we get to that point--}}
 @if(Auth::user())
     <span class="truncate">{{ Auth::user()->currentTeam->name }}</span>
 @else

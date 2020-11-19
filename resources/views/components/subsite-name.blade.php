ff06fa36ad5743b1aeac4edc60fbba41531a3155
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-19 14:42:58 -0500

    tweak

--- resources/views/components/subsite-name.blade.php
+++ resources/views/components/subsite-name.blade.php
@@ -2,8 +2,9 @@
 {{--Have some context here. Basically, if teams are our only tenent, then our team is our subsite.  --}}
 {{--But, if we have tenancy, like a school or club name, then we'll want some different logic--}}
 {{--We'll add the extra subsite logic (somewhere) when we get to that point--}}
+{{--{{ Auth::user()->currentTeam->name }}--}}
 @if(Auth::user())
-    <span class="truncate">{{ Auth::user()->currentTeam->name }}</span>
+    <span class="truncate">TBD Tenant</span>
 @else
     Tenant
 @endif
